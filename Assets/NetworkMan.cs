f85f6158aef63844108ec798a803c269397bdde3
sh960440 <54958110+sh960440@users.noreply.github.com>
2020-10-10 00:26:11 +0800

    Assignment 1

--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -9,61 +9,92 @@ using System.Net;
 public class NetworkMan : MonoBehaviour
 {
     public UdpClient udp;
-    // Start is called before the first frame update
+    public GameObject player;
+
+    public string myAddress;
+    public Dictionary<string, GameObject> currentPlayers;
+    public List<string> newPlayers, droppedPlayers;
+    public GameState latestGameState;
+    public ListOfPlayers initialSetOfPlayers;
+
+    public MessageType latestMessage;
+    
+
+    
     void Start()
     {
+        // Initialize variables
+        newPlayers = new List<string>();
+        droppedPlayers = new List<string>();
+        currentPlayers = new Dictionary<string, GameObject>();
+        initialSetOfPlayers = new ListOfPlayers();
+
+        // Connect to client
         udp = new UdpClient();
+        Debug.Log("Connecting..."); 
+        //udp.Connect("localhost", 12345);
+        udp.Connect("3.96.206.57", 12345);
         
-        udp.Connect("PUT_IP_ADDRESS_HERE",12345);
-
         Byte[] sendBytes = Encoding.ASCII.GetBytes("connect");
-      
         udp.Send(sendBytes, sendBytes.Length);
-
         udp.BeginReceive(new AsyncCallback(OnReceived), udp);
 
-        InvokeRepeating("HeartBeat", 1, 1);
+        InvokeRepeating("HeartBeat", 1, 1/30f);
     }
 
     void OnDestroy(){
         udp.Dispose();
     }
 
-
-    public enum commands{
-        NEW_CLIENT,
-        UPDATE
-    };
-    
     [Serializable]
-    public class Message{
-        public commands cmd;
+    public struct Position
+    {
+        public float x;
+        public float y;
+        public float z;
     }
-    
+
     [Serializable]
     public class Player{
-        [Serializable]
-        public struct receivedColor{
-            public float R;
-            public float G;
-            public float B;
-        }
         public string id;
-        public receivedColor color;        
+        public Position position;
     }
 
     [Serializable]
-    public class NewPlayer{
+    public class ListOfPlayers{
+        public Player[] players;
         
+        public ListOfPlayers() {
+            players = new Player[0];
+        }
+    }
+
+    [Serializable]
+    public class ListOfDroppedPlayers
+    {
+        public string[] droppedPlayers;
     }
 
     [Serializable]
     public class GameState{
+        public int pktID;
         public Player[] players;
     }
 
-    public Message latestMessage;
-    public GameState lastestGameState;
+
+    [Serializable]
+    public class MessageType{
+        public commands cmd;
+    }
+
+    public enum commands{
+        PLAYER_CONNECTED,
+        GAME_UPDATE,
+        PLAYER_DISCONNECTED,
+        CONNECTION_APPROVED,
+        LIST_OF_PLAYERS
+    };
+
     void OnReceived(IAsyncResult result){
         // this is what had been passed into BeginReceive as the second parameter:
         UdpClient socket = result.AsyncState as UdpClient;
@@ -76,18 +107,41 @@ public class NetworkMan : MonoBehaviour
         
         // do what you'd like with `message` here:
         string returnData = Encoding.ASCII.GetString(message);
-        Debug.Log("Got this: " + returnData);
+        //Debug.Log("Got this: " + returnData);
         
-        latestMessage = JsonUtility.FromJson<Message>(returnData);
+        latestMessage = JsonUtility.FromJson<MessageType>(returnData);
+        Debug.Log(returnData);
         try{
             switch(latestMessage.cmd){
-                case commands.NEW_CLIENT:
+                case commands.PLAYER_CONNECTED:
+                    ListOfPlayers latestPlayer = JsonUtility.FromJson<ListOfPlayers>(returnData);
+                    foreach (Player player in latestPlayer.players) {
+                        newPlayers.Add(player.id);
+                    }
                     break;
-                case commands.UPDATE:
-                    lastestGameState = JsonUtility.FromJson<GameState>(returnData);
+                case commands.GAME_UPDATE:
+                    latestGameState = JsonUtility.FromJson<GameState>(returnData);
+                    break;
+                case commands.PLAYER_DISCONNECTED:
+                    Debug.Log(returnData);
+                    ListOfDroppedPlayers latestDroppedPlayer = JsonUtility.FromJson<ListOfDroppedPlayers>(returnData);
+                    foreach (string player in latestDroppedPlayer.droppedPlayers) {
+                        droppedPlayers.Add(player);
+                    }
+                    break;
+                case commands.CONNECTION_APPROVED:
+                    ListOfPlayers myPlayer = JsonUtility.FromJson<ListOfPlayers>(returnData);
+                    Debug.Log(returnData);
+                    foreach (Player player in myPlayer.players) {
+                        newPlayers.Add(player.id);
+                        myAddress = player.id;
+                    }
+                    break;
+                case commands.LIST_OF_PLAYERS:
+                    initialSetOfPlayers = JsonUtility.FromJson<ListOfPlayers>(returnData);
                     break;
                 default:
-                    Debug.Log("Error");
+                    Debug.Log("Error: " + returnData);
                     break;
             }
         }
@@ -99,20 +153,56 @@ public class NetworkMan : MonoBehaviour
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
 
-    void SpawnPlayers(){
+    void SpawnPlayers() {
+        if (newPlayers.Count > 0) {
+            foreach (string playerID in newPlayers) {
+                currentPlayers.Add(playerID, Instantiate(player, new Vector3(0, 0, 0), Quaternion.identity));
+                currentPlayers[playerID].name = playerID;
+            }
+            newPlayers.Clear();
+        }
 
+       if (initialSetOfPlayers.players.Length > 0) {
+            Debug.Log(initialSetOfPlayers);
+            foreach (Player player in initialSetOfPlayers.players) {
+                if (player.id == myAddress) 
+                    continue;
+
+                currentPlayers.Add(player.id, Instantiate(this.player, new Vector3(0, 0, 0), Quaternion.identity));
+                currentPlayers[player.id].name = player.id;
+            }
+            initialSetOfPlayers.players = new Player[0];
+        }
     }
 
     void UpdatePlayers(){
+       
 
+        if (latestGameState.players.Length > 0) {
+            foreach (NetworkMan.Player player in latestGameState.players) {
+                if (myAddress != player.id) {
+                    currentPlayers[player.id].transform.position = new Vector3(player.position.x, player.position.y, player.position.z);
+                }
+            }
+            latestGameState.players = new Player[0];
+        }
     }
 
-    void DestroyPlayers(){
+    void DestroyPlayers() {
+        if (droppedPlayers.Count > 0) {
 
+            foreach (string playerID in droppedPlayers) {
+                Debug.Log(playerID);
+                Debug.Log(currentPlayers[playerID]);
+                Destroy(currentPlayers[playerID].gameObject);
+                currentPlayers.Remove(playerID);
+            }
+        }
+        droppedPlayers.Clear();
     }
     
     void HeartBeat(){
-        Byte[] sendBytes = Encoding.ASCII.GetBytes("heartbeat");
+        Byte[] sendBytes = Encoding.ASCII.GetBytes(JsonUtility.ToJson(currentPlayers[myAddress].transform.position));
         udp.Send(sendBytes, sendBytes.Length);
     }
 
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movementVector *= Time.deltaTime * 5;
+
+        transform.Translate(movementVector);
+    }
+
+
+}
